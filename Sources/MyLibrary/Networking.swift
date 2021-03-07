8ee28d1054204a8f6a9e63137cece8edf79d51e7
candrarestu24 <candra.restu@gli.id>
2021-03-08 04:13:47 +0700

    add post data

--- Sources/MyLibrary/Networking.swift
+++ Sources/MyLibrary/Networking.swift
@@ -9,9 +9,17 @@ import Foundation
 
 protocol NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void)
 }
 
 extension URLSession: NetworkSession {
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        let task = dataTask(with: request) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         let task = dataTask(with: url) { data, _, error in
             completionHandler(data, error)
@@ -38,6 +46,21 @@ extension MyLibrary {
                 session.get(from: url) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
+                }
+            }
+            
+            public func sendData<I: Codable>(to url: URL, body: I, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                var request = URLRequest(url: url)
+                do {
+                    let httpBody = try JSONEncoder().encode(body)
+                    request.httpBody = httpBody
+                    request.httpMethod = "POST"
+                    session.post(with: request) { data, error in
+                        let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                        completionHandler(result)
+                    }
+                } catch let error {
+                    return completionHandler(.failure(error))
                 }
             }
         }
--- Tests/MyLibraryTests/MyLibraryNetworkingTest.swift
+++ Tests/MyLibraryTests/MyLibraryNetworkingTest.swift
@@ -9,6 +9,7 @@ import XCTest
 @testable import MyLibrary
 
 class NetworkSessionMock: NetworkSession {
+
     var data: Data?
     var error: Error?
     
@@ -16,7 +17,15 @@ class NetworkSessionMock: NetworkSession {
         completionHandler(data, error)
     }
     
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
     
+}
+
+struct MockData: Codable, Equatable {
+    var id: Int
+    var name: String
 }
 
 final class MyLibraryNetworkingTest: XCTestCase {
@@ -34,6 +43,29 @@ final class MyLibraryNetworkingTest: XCTestCase {
             switch result {
             case .success(let returnedData):
                 XCTAssertEqual(data, returnedData, "manager returned unexpected data")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = MyLibrary.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "test")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "sent Data")
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
+                break
             case .failure(let error):
                 XCTFail(error?.localizedDescription ?? "error forming error result")
             }
